18e43d16c1f57a5da57da36d193fbc55a12fb079
Matthew Menna <mattmenna@gmail.com>
2017-06-28 09:47:25 -0400

    Structure of switch added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,35 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         String name;
         int caseNumber;
+        char playGame;
 
-        switch (caseNumber){
-            case:
-        }
-    }
+        Scanner scnr = new Scanner(System.in);
 
-    //private String
-}
+        System.out.println("Welcome! What is your name?");
+        name = scnr.nextLine();
+        System.out.println("Hi " + name);
+        System.out.println("Would you like to play a game?");
+        playGame = scnr.nextLine().charAt(0);
+
+        if (playGame == 'y') {
+
+
+            switch (caseNumber) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+            } // end Switch
+        } // game loop
+
+    } // end Main
+
+} // end Class
